41bdc9f63c271e77b0e11f37ca636ee30f912274
Sukhdev <48499132+algojedi@users.noreply.github.com>
2025-08-07 14:44:07 -0400

    feat: reactive form created for todo creation

--- src/app/features/todos/create/todo-create.component.ts
+++ src/app/features/todos/create/todo-create.component.ts
@@ -1,33 +1,59 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-
+import { Todo } from '../types';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-create',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './todo-create.component.html',
   styleUrl: './todo-create.component.scss',
 })
 export class TodoCreateComponent {
-  title = signal('');
-  description = signal('');
+  private fb = inject(FormBuilder);
 
-  isValid = computed(
-    () =>
-      this.title().trim().length > 0 && this.description().trim().length > 0,
-  );
+  form = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', [Validators.required, Validators.minLength(5)]],
+  });
+
+  ngOnInit() {
+    this.form.statusChanges.subscribe(() => {
+      for (const controlName in this.form.controls) {
+        const control = this.form.get(controlName);
+        console.log({ walue: control?.value });
+        console.log({ isValid: this.form.valid });
+        if (control?.invalid) {
+          console.log(`Control "${controlName}" errors:`, control.errors);
+        }
+      }
+    });
+  }
 
   onSubmit() {
-    const todo = {
-      title: this.title(),
-      description: this.description(),
+    const { title, description } = this.form.value;
+    const todo: Todo = {
+      title: title!,
+      description: description!,
+      completed: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      id: crypto.randomUUID(), // Generate a unique ID for the new todo
     };
 
     console.log('Creating todo:', todo);
-    // Call a service to persist it
+    // save on session storage as key of todo
+    localStorage.setItem('test', 'I hope this works');
+    sessionStorage.setItem(todo.title, JSON.stringify(todo));
   }
 }
