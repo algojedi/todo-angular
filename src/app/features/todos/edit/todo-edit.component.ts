1f0528c66f61fac61a3697620044edc1a697b593
Sukhdev <48499132+algojedi@users.noreply.github.com>
2025-08-08 12:43:22 -0400

    feat: add edit functionality

--- src/app/features/todos/edit/todo-edit.component.ts
+++ src/app/features/todos/edit/todo-edit.component.ts
@@ -1,13 +1,24 @@
 import { Component, inject, signal } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Todo } from '../types';
 import { TodoService } from '../todo.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-todo-edit',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './todo-edit.component.html',
   styleUrl: './todo-edit.component.scss',
   template: `
@@ -19,9 +30,14 @@ export class TodoEditComponent {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private id = signal(this.route.snapshot.paramMap.get('id'));
-
+  private previousTodo = signal<Todo | null>(null);
   private readonly todoService = inject(TodoService);
-  // private todo: Todo; // Prefer typing over 'any'
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', [Validators.required, Validators.minLength(5)]],
+  });
 
   ngOnInit(): void {
     this.loadTodo();
@@ -34,12 +50,26 @@ export class TodoEditComponent {
       this.router.navigate(['/not-found']);
       return;
     }
-    let todo
     this.todoService.getTodo(id).subscribe({
       next: (response) => {
         console.log('Todo:', response);
-        todo = response.data;
-        console.log({ todo });
+        this.previousTodo.set(response.data)
+        console.log({ todo : this.previousTodo() });
+        // populate the form with the todo data
+        this.form.patchValue({
+          title: this.previousTodo()!.title,
+          description: this.previousTodo()!.description,
+        });
+        this.form.statusChanges.subscribe(() => {
+          for (const controlName in this.form.controls) {
+            const control = this.form.get(controlName);
+            console.log({ value: control?.value });
+            console.log({ isValid: this.form.valid });
+            if (control?.invalid) {
+              console.log(`Control "${controlName}" errors:`, control.errors);
+            }
+          }
+        });
       },
       error: (error) => {
         console.error('Error fetching todos:', error);
@@ -47,4 +77,32 @@ export class TodoEditComponent {
       },
     });
   }
+
+  onSubmit() {
+    const { title, description } = this.form.value;
+    const todo: Todo = {
+      title: title!,
+      description: description!,
+      completed: false,
+      createdAt: this.previousTodo()!.createdAt, 
+      updatedAt: new Date(),
+      id: this.previousTodo()!.id 
+    };
+
+    this.todoService.updateTodo(+todo.id, todo).subscribe({
+      next: (response) => {
+        console.log('Todo updated successfully:', response);
+        this.router.navigate(['/todos']);
+      },
+      error: (error) => {
+        console.error('Error updating todo:', error);
+      },
+    });
+
+    console.log('Creating todo:', todo);
+    // save on session storage as key of todo
+    localStorage.setItem('test', 'I hope this works');
+    sessionStorage.setItem(todo.title, JSON.stringify(todo));
+  }
+
 }
--- src/app/features/todos/todo.service.ts
+++ src/app/features/todos/todo.service.ts
@@ -14,6 +14,10 @@ export class TodoService {
   getTodo(id: string | number) {
     return this.http.get<GetTodoResponse>(api.todos.getById(id));
   }
+  // create function for updating a todo
+  updateTodo(id: number, todo: Todo) {
+    return this.http.put<Todo>(api.todos.update(id), todo);
+  }
 
   deleteTodo(id: number) {
     return this.http.delete(api.todos.delete(id));
